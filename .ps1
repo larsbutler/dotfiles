8c94093d3dcdd78e7e5c4c6b47ec6530bb5ac064
Lars Butler <Lars.Butler@gmail.com>
2012-03-07 10:25:07 +0100

    updated a directory path

--- .ps1
+++ .ps1
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ~/env-configs/.shell_colors
+source .shell_colors
 GIT_PS1='\[\033[1;33m\]`__git_ps1`'
 export PS1="$WHITE\u$PURPLE@$CYAN\h: $LIGHT_GREEN\w$GIT_PS1 $LIGHT_GRAY\\$ "
