d1b59b163a2cc8c1bb48f24f874f7becb3a4e101
Lars Butler <Lars.Butler@gmail.com>
2015-10-05 10:06:27 +0200

    Fix use of __git_ps1 in PS1 var
    Prevents spurious errors, particularly when switching to another user
    (like root).

--- .ps1
+++ .ps1
@@ -1,4 +1,10 @@
 #!/bin/bash
 source .ps1_colors
-GIT_PS1='\[\033[1;33m\]`__git_ps1`'
+# If __git_ps1 is defined, then customize the prompt and include git status.
+# Else, stub it out with empty contents.
+if [ -n `command -v __git_ps1` ]; then
+    GIT_PS1='\[\033[1;33m\]`__git_ps1`'
+else
+    GIT_PS1=''
+fi
 export PS1="$WHITE\u$PURPLE@$CYAN\h: $LIGHT_GREEN\w $GIT_PS1 $LIGHT_GRAY\\$ "
