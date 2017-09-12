abd78c6ac2e888b87a0a4f7127d06967400440a0
Lars Butler <Lars.Butler@gmail.com>
2017-09-12 10:36:50 +0200

    .ps1: nicer ps1 with newline, exit code display

--- .ps1
+++ .ps1
@@ -7,4 +7,24 @@ if [ -n `command -v __git_ps1` ]; then
 else
     GIT_PS1=''
 fi
-export PS1="$WHITE\u$PURPLE@$CYAN\h: $LIGHT_GREEN\w $GIT_PS1 $LIGHT_GRAY\\$ "
+
+
+__prompt_command() {
+    local EXIT_CODE="$?"
+    PS1=""
+    PS1="$WHITE\u$PURPLE@$CYAN\h: $LIGHT_GREEN\w $GIT_PS1 $LIGHT_GRAY\n"
+    if [ $EXIT_CODE == 0 ]; then
+        # Green 0
+        PS1+="${GREEN}[0]${LIGHT_GRAY}"
+    else
+        # Red <whatever the code is>
+        PS1+="${RED}[${EXIT_CODE}]${LIGHT_GRAY}"
+    fi
+
+    # Finish the prompt:
+    PS1+=" $ "
+
+    export PS1=${PS1}
+    EXIT_CODE=0
+}
+export PROMPT_COMMAND="__prompt_command;update_terminal_cwd"
